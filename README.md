b91972113c9219404566a94c6eb22836bb11d5a5
nagendrajimaninta <jimanintanagendra01@gmail.com>
2022-03-14 23:53:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-gw7fax
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gw7fax)
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+class App1 extends React.Component{
+  state={
+    counter:0
+  };
+  increment =()=>{
+    this.setState({counter:this.state.counter+1});
+  }
+  decrement =()=>{
+    this.setState({counter:this.state.counter-1});
+  }
+  
+    render(){
+      return (
+        <>
+        <h1>counter is : {this.state.counter}</h1>
+        <button onClick={this.increment}>Inc</button>
+        <button onClick={this.decrement}>Dec</button>
+        </>
+      );
+    }
+}
+export default function App() {
+  return (
+    <div>
+      <App1 />
+      <h1>Above is the counter : By using class component</h1>
+      <p>Class component having a props as a arguments</p>
+      <App1 />
+    </div>
+  );
+}
